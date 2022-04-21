f1c40814fada2f63a512ab7a0e8292441c308f0e
GitHub Action <action@github.com>
2022-04-21 12:24:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/paukre/pilotgame/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paukre/pilotgame/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paukre.pilotgame.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paukre.pilotgame.MyBundle
 
 class MyProjectService(project: Project) {
 
